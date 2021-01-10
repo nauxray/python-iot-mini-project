b74135f53685078e344d542103238b16f1ce4d31
nauxray <xuan.yar@gmail.com>
2021-01-11 05:50:40 +0800

    flask code

--- init.py
+++ init.py
@@ -1,1 +1,13 @@
-hihi
+# from flask import Flask
+# app=Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return "Hello World"
+
+# @app.route('/page2')
+# def page2():
+#     return "second page"
+
+# if __name__ == "__main__":
+#     app.run(debug=True,host='0.0.0.0') 
