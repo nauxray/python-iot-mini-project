40089b1c2f530cba360ebfda304e1d612af04b0f
Melvin Teo <melvin_teo@hotmail.sg>
2021-01-12 09:13:11 +0800

    hi

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-from flask import Flask
-app=Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Hello World"
-
-@app.route('/page2')
-def page2():
-    return "second page"
-
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0') 
+import time
