a4c515408d8b7d8c189bb6d4a47bcd9c8b5da672
nauxray <xuan.yar@gmail.com>
2021-02-13 02:00:19 +0800

    telegram connected

--- testapp.py
+++ testapp.py
@@ -2,10 +2,44 @@ import time
 import requests
 from threading import Thread
 
+from telethon.sync import TelegramClient 
+from telethon.tl.types import InputPeerUser, InputPeerChannel 
+from telethon import TelegramClient
+
+api_id = '2616181'
+api_hash = 'a2c456736c48db4a64cf944634eaf3c4'
+token = '1582053448:AAFSoZpF0i5TWq3ECP0sBtI6apabD5C02so'
 runtimeArr = []
+
+phone = '+65' + input("Enter phone number (yx is 92973342): ")
+print("PHONE NUMBER:", phone)
 
 print(f'{"":-^40}')
 print(f'{"It is not raining :)": ^40}')
+
+def sendMsg():
+    # creating a telegram session and assigning 
+    # it to a variable client 
+    client = TelegramClient('session', api_id, api_hash) 
+    
+    # connecting and building the session 
+    client.connect() 
+    
+    # in case of script ran first time it will ask either to input token or otp sent to number or sent or your telegram id  
+    if not client.is_user_authorized(): 
+        client.send_code_request(phone) 
+        # signing in the client 
+        client.sign_in(phone, input('Enter the code: ')) 
+    try: 
+        # melvin's userinfo inserted to send msg to him
+        receiver = InputPeerUser(client.get_entity("BobaSucker").id, client.get_entity("BobaSucker").access_hash)     
+        # sending message using telegram client 
+        client.send_message(receiver, "ALERT ALERT", parse_mode='html') 
+    except Exception as e: 
+        print(e);     
+    # disconnecting the telegram session  
+    client.disconnect() 
+
 
 def moistureCheck():
     if input("Moisture check y/n?\n>> ") == "y":
@@ -32,13 +66,14 @@ def buzzerAlert():
 
 def moistureLoop():
     while True:
-        stopLoop = input("Do you want to stop testing moisture loop? y/n\n>> ")
+        stopLoop = input("Stop testing moisture loop? y/n\n>> ")
         if stopLoop == "y":
             break
         else:
             if moistureCheck() == True:
                 print(f'{"":-^40}')
                 print("Alert to phone")
+                sendMsg()
                 print("It is raining :(")
                 print(f'{"":-^40}')
                 if switchCheck() == True:
@@ -71,7 +106,7 @@ def inputLoop():
             print(f'Runtime: {runtime}, Date: {time.strftime("%d-%m-%Y")}')
             print(f'{"":-^40}')
             runtime = ''
-        testRuntime = input("U want to test the runtime loop? y/n\n>> ")
+        testRuntime = input("Test the runtime loop? y/n\n>> ")
         if testRuntime == "n":
             break
 
@@ -82,6 +117,6 @@ while True:
         break
 
     moistureLoop()
-    testRuntime = input("U want to test the runtime loop? y/n\n>> ")
+    testRuntime = input("Test the runtime loop? y/n\n>> ")
     if testRuntime == "y":
         inputLoop()
